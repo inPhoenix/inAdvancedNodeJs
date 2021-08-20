5c3a7acec92f8bc06c8f53e66fb722838ef6ccd4
inPhoenix <spiderk@gmail.com>
2021-08-20 08:50:10 -0300

    Put everything in its right place

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,9 @@
+const devKey = require('../secrets')
+
 module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: '',
+  mongoURI: devKey,
   cookieKey: '123123123',
 };
